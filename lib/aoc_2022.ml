7a8c8df7f0e37d2924aff40fa1eeb324db437cca
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-11-02 18:48:50 -0300

    Make code to handle file input more generic

---
+++ lib/aoc_2022.ml
@@ -0,0 +1,1 @@
+module Calorie_counting = Calorie_counting
--- lib/calorie_counting.ml
+++ lib/calorie_counting.ml
@@ -1,7 +1,3 @@
-let read_lines file =
-  In_channel.with_open_bin file In_channel.input_all
-  |> String.split_on_char '\n'
-
 let split_inventories lines =
   let rec aux xs ys curr =
     match xs with
@@ -15,12 +11,12 @@ let sum = List.fold_left (+) 0
 
 let max list = List.fold_left Int.max (List.hd list) list
 
-let () =
-  let input_file = "input/day_01_calorie_counting.txt" in
-  input_file
-  |> read_lines
+let solve file_contents =
+  file_contents
+  |> String.split_on_char '\n'
   |> split_inventories
   |> List.map (List.map int_of_string)
   |> List.map sum
   |> max
   |> Printf.printf "%d\n"
+
