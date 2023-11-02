dab11b2c8a54f7254d01f705ec82d3aaa6a2f1b8
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-11-02 18:57:41 -0300

    Turn args() func into a module Cmd

--- bin/main.ml
+++ bin/main.ml
@@ -2,22 +2,24 @@ open Aoc_2022
 
 let read_file file = In_channel.with_open_bin file In_channel.input_all
 
+module Cmd = struct
+  type t = { challenge : string; input_file : string }
+
+  let parse () =
+    match Array.to_list Sys.argv with
+    | [ _; challenge; input_file ] -> { challenge; input_file }
+    | _ ->
+        Printf.printf "Usage: aoc_2022 <challenge> <input_file>\n";
+        invalid_arg "Invalid number of arguments received"
+end
+
 let select_challenge = function
-  | "calorie_counting" -> Ok (Calorie_counting.solve)
+  | "calorie_counting" -> Ok Calorie_counting.solve
   | challenge -> Error (Printf.sprintf "Unknown challenge: %s" challenge)
 
-let args () =
-  match Array.to_list Sys.argv with
-  | [_; challenge; input_file] -> (challenge, input_file)
-  | _ ->
-     Printf.printf "Usage: aoc_2022 <challenge> <input_file>\n";
-     invalid_arg "Invalid number of arguments received"
-
 let () =
-  let (challenge, input_file) = args () in
+  let { Cmd.challenge; input_file } = Cmd.parse () in
   let file_contents = read_file input_file in
   match select_challenge challenge with
   | Ok solve_fn -> solve_fn file_contents
   | Error msg -> invalid_arg msg
-
-
