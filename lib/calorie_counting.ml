ba2967d443931fd51adf30f82bba722e977f14dc
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-11-02 23:35:09 -0300

    Add expect test for day 1

--- lib/calorie_counting.ml
+++ lib/calorie_counting.ml
@@ -14,9 +14,30 @@ let max list = List.fold_left Int.max (List.hd list) list
 let solve file_contents =
   file_contents
   |> String.split_on_char '\n'
+  |> List.map String.trim
   |> split_inventories
   |> List.map (List.map int_of_string)
   |> List.map sum
   |> max
   |> Printf.printf "%d\n"
+
+let%expect_test _ =
+  solve {|
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000|};
+  [%expect {|
+    24000
+    |}]
 
