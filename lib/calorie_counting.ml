09edd4148e58fe356f4119d8c2361c4b9ec2860a
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-11-03 00:07:48 -0300

    Add part 2 of day 1

--- lib/calorie_counting.ml
+++ lib/calorie_counting.ml
@@ -11,15 +11,36 @@ let sum = List.fold_left (+) 0
 
 let max list = List.fold_left Int.max (List.hd list) list
 
+let top_3_elves inventories =
+  let calories =
+    inventories
+    |> List.map sum
+    |> List.sort Int.compare
+    |> List.rev
+  in
+  match calories with
+  | f :: s :: t :: _ -> Some (f, s, t)
+  | _ -> None
+
 let solve file_contents =
-  file_contents
-  |> String.split_on_char '\n'
-  |> List.map String.trim
-  |> split_inventories
-  |> List.map (List.map int_of_string)
+  let inventories =
+    file_contents
+    |> String.split_on_char '\n'
+    |> List.map String.trim
+    |> split_inventories
+    |> List.map (List.map int_of_string)
+  in
+
+  (* Part 1 *)
+  inventories
   |> List.map sum
   |> max
-  |> Printf.printf "%d\n"
+  |> Printf.printf "%d\n";
+
+  (* Part 2 *)
+  match top_3_elves inventories with
+  | Some (f, s, t) -> Printf.printf "%d\n" (f + s + t)
+  | None -> invalid_arg "Input should have at least 3 elf inventories"
 
 let%expect_test _ =
   solve {|
@@ -36,8 +57,9 @@ let%expect_test _ =
     8000
     9000
 
-    10000|};
+    10000
+    |};
   [%expect {|
     24000
+    45000
     |}]
-
